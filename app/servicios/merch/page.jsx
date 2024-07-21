0bf206cc12be8fc1d7a3074a1ce02fbe87bb3634
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-07-20 23:20:46 -0500

    merch page

--- app/servicios/merch/page.jsx
+++ app/servicios/merch/page.jsx
@@ -1,7 +1,34 @@
-export default function MerchPage(){
-    return(
-        <>
-        <h1>Página de Navegación de Servicios: Merchandising</h1>
-        </>
-    );
+import NavBarItem from "@/app/components/NavbarItem";
+
+const merchItems = [
+  { image: "/buzos-merch.jpg", text: "BUZOS" },
+  { image: "/polos-merch.jpg", text: "POLOS" },
+  { image: "/poleras-merch.jpg", text: "POLERAS" },
+  { image: "/gorras-merch.jpg", text: "GORRAS" },
+  { image: "/mochilas-merch.jpg", text: "MOCHILAS" },
+  { image: "/canguros-merch.jpg", text: "CANGUROS" },
+  { image: "/folders-merch.jpg", text: "FOLDERS" },
+  { image: "/maletines-merch.jpg", text: "MALETINES" },
+  { image: "/peluches-merch.jpg", text: "PELUCHE" },
+];
+
+export default function MerchPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-8 mt-8">
+        Ventas y merchandising
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {merchItems.map((item, index) => (
+          <NavBarItem
+            key={index}
+            href=""
+            src={item.image}
+            alt={item.text}
+            title={item.text}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/servicios/prestamos/page.jsx
+++ app/servicios/prestamos/page.jsx
@@ -1,7 +1,31 @@
-export default function PrestamosPage(){
-    return(
-        <>
-        <h1>Página de Navegación de Servicios: Préstamos</h1>
-        </>
-    );
+import NavBarItem from "@/app/components/NavbarItem";
+
+const prestamosItems = [
+  { href: "/prestamos/solo-firma", image: "/solofirma-prestamos.jpg", text: "Sola Firma" },
+  { href: "/prestamos/ordinario", image: "/ordinario-prestamos.jpg", text: "Ordinario" },
+  { href: "/prestamos/promocional", image: "/promocional-prestamos.jpg", text: "Promocional" },
+  { href: "/prestamos/campaña-escolar", image: "/campaña-escolar-prestamos.jpeg", text: "Campaña escolar" },
+  { href: "/prestamos/campaña-fiestas-patrias", image: "/campaña-fiestas-patrias-prestamos.jpg", text: "Campaña fiestas patrias" },
+];
+
+export default function PrestamosPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-black flex items-center justify-center m-10">
+        Prestamos
+      </h1>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-4 grid-rows-1 gap-10 place-items-center max-w-xxl mx-auto">
+          {prestamosItems.map((item, index) => (
+            <NavBarItem
+              key={index}
+              href={item.href}
+              src={item.image}
+              title={item.text}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
