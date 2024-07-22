5a584c61c69cd074c84c0e34bb0e3980427625b1
Gterxa <gcuray123@gmail.com>
2024-07-21 19:37:19 -0500

    Page: Contacto

--- app/components/FooterItem.jsx
+++ app/components/FooterItem.jsx
@@ -2,7 +2,7 @@ export default function FooterItem({ children, title }) {
   return (
     <>
       <div className="flex flex-col items-center">
-        <h3 className="bg-[#209F63] w-96 flex justify-center mt-5 py-1">
+        <h3 className="bg-[#209F63] w-80 flex justify-center mt-5 py-1">
           {title}
         </h3>
         <div className="pl-5 py-3 text-xs">{children}</div>
--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -1,7 +1,121 @@
-export default function ContactNav(){
-    return(
-        <>
+import Image from "next/image";
+
+export default function ContactNav() {
+  return (
+    <>
+      <div>
         <h1>Página del Navbar: Contacto</h1>
-        </>
-    )
+        <div className="flex flex-col lg:flex-row m-8 p-4 gap-8 rounded-lg shadow-lg">
+          <div>
+            <Image src="/contacto.png" height={900} width={1200} alt="holi" />
+          </div>
+          <div className="flex flex-col gap-3 mt-8">
+            <p>
+              Nuestra oficina principal se encuentra ubicada en el Jr.
+              Pachacutec 2052 - Jesús María - Lima, como referencia estamos en
+              el cruce de la Av. Salaverry con la Av. San Felipe, en la entrada
+              al Circulo Militar.
+            </p>
+            <p>Nuestros datos principales para contacto, son los siguientes:</p>
+            <p className="ml-12">
+              - Teléfonos 988440963 / 471-6022 / 471-7135 Fax 471 3364
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <table className="m-3 text-1xs md:text-sm lg:text-base border-black border shadow-lg">
+            <thead className="bg-[#BDD287] border-black border">
+              <tr>
+                <th className="border-black border">Oficinas</th>
+                <th className="border-black border">Operaciones</th>
+                <th className="border-black border">Responsable</th>
+                <th className="border-black border">Correo</th>
+                <th className="border-black border">Nros.Whatsapp</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-black border">
+                <td className="border-black border pl-1">Cuentas Corrientes</td>
+                <td className="border-black border pl-1">
+                  Retiro de Ahorros
+                  <br />
+                  Retiro de Pago Anticipado
+                </td>
+                <td className="border-black border pl-1">Diana Becerra</td>
+                <td className="border-black border pl-1 text-blue-900">
+                  <a href="mailto:ctasctes@alaspe.pe">ctasctes@alaspe.pe</a>
+                </td>
+                <td className="border-black border pl-1">982 809 650</td>
+              </tr>
+              <tr className="border-black border">
+                <td className="border-black border pl-1">
+                  Crédito y morosidad
+                </td>
+                <td className="border-black border pl-1">
+                  Préstamos ordinarios
+                  <br />
+                  Préstamos a sola firma
+                </td>
+                <td className="border-black border pl-1">
+                  Lourdes Castillo
+                  <br />
+                  Emilio Guzman
+                </td>
+                <td className="border-black border pl-1 text-blue-900">
+                  <a href="mailto:credito@alaspe.pe">credito@alaspe.pe</a>
+                </td>
+                <td className="border-black border pl-1">989 142 046</td>
+              </tr>
+              <tr className="border-black border">
+                <td className="border-black border pl-1">Cobranzas</td>
+                <td className="border-black border pl-1">
+                  Gestión de cobranzas
+                </td>
+                <td className="border-black border pl-1">Emilio Guzman</td>
+                <td className="border-black border pl-1 text-blue-900">
+                  <a href="mailto:cobranza@alaspe.pe">cobranza@alaspe.pe</a>
+                </td>
+                <td className="border-black border pl-1">982 559 829</td>
+              </tr>
+              <tr className="border-black border">
+                <td className="border-black border pl-1">Previsión Social</td>
+                <td className="border-black border pl-1">
+                  Consulta de trámites para Subsidios por Fallecimiento de
+                  Socios y Familiares
+                </td>
+                <td className="border-black border pl-1">Rosario Contreras</td>
+                <td className="border-black border pl-1 text-blue-900">
+                  <a href="mailto:prevision@alaspe.pe">prevision@alaspe.pe</a>
+                </td>
+                <td className="border-black border pl-1">989 142 055</td>
+              </tr>
+              <tr>
+                <td className="border-black border pl-1">Educación</td>
+                <td className="border-black border pl-1">
+                  Admisión de Socios Nuevos
+                  <br />
+                  Trámites para Renuncia
+                </td>
+                <td className="border-black border pl-1">Jesús Paredes</td>
+                <td className="border-black border pl-1 text-blue-900">
+                  <a href="mailto:educacion@alaspe.pe">educacion@alaspe.pe</a>
+                </td>
+                <td className="border-black border pl-1">989 142 051</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-between my-12 mx-4 md:mx-24">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2758.7331474307234!2d-77.04555865686561!3d-12.082150300249362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8f9e96bd5ad%3A0x1b821a39eb044ba8!2sJir%C3%B3n%20Pachacutec%202052%2C%20Jes%C3%BAs%20Mar%C3%ADa%2015072!5e0!3m2!1ses-419!2spe!4v1686416056572!5m2!1ses-419!2spe"
+            width="100%"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
 }
