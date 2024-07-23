c8e3f283bbc9bb3d7bef49c000dc869c7a8f9225
Manuel Zambrano <manuel.nazaret.zambrano.moran@gmail.com>
2024-07-23 00:41:11 -0500

    Nosotros page

--- app/comunicado/page.jsx
+++ app/comunicado/page.jsx
@@ -1,7 +1,33 @@
-export default function ComunicNav(){
-    return(
-        <>
-        <h1>Página del Navbar: Comunicado</h1>
-        </>
-    )
+import NavBarItem from "../components/NavbarItem";
+
+export default function ComunicNav() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center pt-5 gap-12">
+        <h1 className="text-4xl font-bold text-black flex items-center justify-center m-10">
+          Comunicados
+        </h1>
+        <div className="flex flex-wrap justify-center gap-12">
+          <NavBarItem
+            href="/comunicado/historial"
+            src="/hospedaje-logo.webp"
+            alt="Historial"
+            title="Historial"
+          />
+          <NavBarItem
+            href="/comunicado/socios-camip"
+            src="/merch-logo.webp"
+            alt="Socio CAMIP"
+            title="Socios CAMIP"
+          />
+          <NavBarItem
+            href="/comunicado/pagos-en-bancos"
+            src="/prestamos-logo.webp"
+            alt="Pagos en bancos"
+            title="Pagos en bancos"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
