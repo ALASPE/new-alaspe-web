a30d249af75a6184dc7c6cf36fe4d1a17fd7da63
Gterxa <gcuray123@gmail.com>
2024-07-26 11:12:17 -0500

    Slider

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Popup from "./components/Popup";
-import SwiperCarousel from "./components/Slider";
-import PageComp from "./components/PageComp";
-import NavBarItem from "./components/NavbarItem";
+import ImageSlider from "./components/Slider";
 
 const images = [
   "/slider/Slider1@10x.png",
@@ -21,72 +18,7 @@ const images = [
 export default function HomePage() {
   return (
     <>
-      <Popup />
-      <div className="lg:mx-12">
-        <SwiperCarousel images={images} w="2500" h={600} />
-        <PageComp title="Nuestros Servicios, Beneficios y Convenios">
-          <NavBarItem
-            href="/servicios/prestamos"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Préstamos"
-          />
-          <NavBarItem
-            href="/servicios/prevision-social"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Previsión Social"
-          />
-          <NavBarItem
-            href="/servicios/hospedaje"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Hospedaje"
-          />
-          <NavBarItem
-            href="/beneficios/actividades"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Actividades y Cursos"
-          />
-          <NavBarItem
-            href="/convenios/club"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Sorteo Gratuito"
-          />
-          <NavBarItem
-            href="/servicios/merch"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Ventas y Merchandising"
-          />
-          <NavBarItem
-            href="/beneficios/educacion"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Sistema Educativo"
-          />
-          <NavBarItem
-            href="/servicios/curso-cooperativismo"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Curso Cooperativismo"
-          />
-          <NavBarItem
-            href="/beneficios/servicentro"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Servicentro"
-          />
-          <NavBarItem
-            href="/convenios"
-            src="/hospedaje-logo.webp"
-            alt="Actividades logo"
-            title="Convenios"
-          />
-        </PageComp>
-      </div>
+      <ImageSlider images={images}/>
     </>
   );
 }
